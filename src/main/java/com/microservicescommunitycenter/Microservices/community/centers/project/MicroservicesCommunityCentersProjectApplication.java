1acdd90c6f4a4915ad9dc176b9c87e10540a9bbd
EudesGomes017 <eudesgomes017@gmail.com>
2025-07-14 12:32:24 -0300

    feat: create project

---
+++ src/main/java/com/microservicescommunitycenter/Microservices/community/centers/project/MicroservicesCommunityCentersProjectApplication.java
@@ -0,0 +1,13 @@
+package com.microservicescommunitycenter.Microservices.community.centers.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCommunityCentersProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCommunityCentersProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicescommunitycenter/Microservices/community/centers/project/MicroservicesCommunityCentersProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicescommunitycenter.Microservices.community.centers.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesCommunityCentersProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
